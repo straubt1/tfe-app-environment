cf663f6bc8e5a4dbffbdbc013f58b5e7baf00335
Tom Straub <tstraub@hashicorp.com>
2020-02-08 14:52:14 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,15 +3,15 @@ resource "random_uuid" "my-uuid" {}
 
 resource "random_uuid" "new-uuid" {}
 
-variable "name" {}
-variable "location" {}
+# variable "name" {}
+# variable "location" {}
 
-resource "azurerm_resource_group" "module" {
-  name     = "${var.name}-rg"
-  location = "${var.location}"
+# resource "azurerm_resource_group" "module" {
+#   name     = "${var.name}-rg"
+#   location = "${var.location}"
 
-  tags = {
-    environment = "Development"
-    b       = "Tom"
-  }
-}
+#   tags = {
+#     environment = "Development"
+#     b       = "Tom"
+#   }
+# }
